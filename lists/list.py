836804ef286ea3ca49e45a5544bba03c8e49e8db
HellenWanjiru <Hellenwanjirumwaura@gmail.com>
2023-02-10 10:52:22 +0300

    python lists

---
+++ lists/list.py
@@ -0,0 +1,30 @@
+#numbers=[5,10,13,7,10,7]
+#print(numbers.index(7))
+#names = ["python","c++","java"]
+#print(names.index("java"))
+#numbers.append(90)
+#for i in range(len(numbers)):
+ #   print(numbers[i])
+#print(numbers.count(10))
+#print(numbers.pop(2))
+#numbers.insert(2,100)
+#numbers.sort()
+#for i in range(len(numbers)):
+ #  print(numbers[i])
+#zero=[0]*10
+#nums=3,2,10,43,67
+
+#print(min(nums))
+
+#nums=[]
+#for i in range(10):
+ #   nums.append(i)
+
+#for i in range(len(nums)):
+ #   print(nums[i])
+even_nums=[]
+for num in range(20):
+    if((num%2)==0):
+        even_nums.append(num)
+for i in even_nums:
+    print(i)
